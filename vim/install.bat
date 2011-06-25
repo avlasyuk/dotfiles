72623a0edc4ddc229836b113e360bce4b3784594
Andrei <andrei.vlasyuk@gmail.com>
2011-06-25 04:30:57 -0700

    Edited vim/install.bat via GitHub

--- vim/install.bat
+++ vim/install.bat
@@ -6,5 +6,5 @@ set home=%UserProfile%
 del "%home%\_vimrc"
 rmdir "%home%\vimfiles"
 
-mklink /d "%home%\vimfiles" "."
-mklink "%home%\_vimrc" ".\_vimrc"
+mklink /d "%home%\vimfiles" "%cd%"
+mklink "%home%\_vimrc" "%cd%\_vimrc"
