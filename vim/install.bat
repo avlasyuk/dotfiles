8f13257b99354b49719d6974d09c11eb0ee6645e
Andrei Vlasyuk <andrei.vlasyuk@gmail.com>
2012-02-23 14:57:11 +0400

    Moved to Vundle vim addon manager

--- vim/install.bat
+++ vim/install.bat
@@ -1,10 +1,1 @@
-:: Shall be executed with administrator access.
-:: This is the way we can make symlinks with mklink utility.
-
-set home=%UserProfile%
-
-del "%home%\_vimrc"
-rmdir "%home%\vimfiles"
-
-mklink /d "%home%\vimfiles" "%cd%"
-mklink "%home%\_vimrc" "%cd%\vimrc"
+mklink "%UserProfile%\_vimrc" "%cd%\vimrc"
