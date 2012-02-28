0884d7694604ef4664f29d2653355d8239ee9dd7
Andrey Vlasyuk <avlasyuk@yandex-team.ru>
2012-02-28 20:42:41 +0400

    ^_^

---
+++ install.sh
@@ -0,0 +1,9 @@
+rm -f ~/.vimrc
+ln -s "`pwd`/vim/vimrc" ~/.vimrc
+rm -f ~/.bashrc
+ln -s "`pwd`/bash/bashrc" ~/.bashrc
+rm -f ~/.bash_profile
+ln -s "`pwd`/bash/bash_profile" ~/.bash_profile
+rm -f ~/.screenrc
+ln -s "`pwd`/screen/screenrc" ~/.screenrc
+
